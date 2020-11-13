3eb05ebfee87a6870dfe8a2498324c3720e762dd
evaniaintiha <Evania.intiha@yahoo.com>
2020-11-13 12:04:21 +0700

    UTS

---
+++ EmergencyCall/app/src/main/java/com/example/emergencycall/rumah_sakit.java
@@ -0,0 +1,14 @@
+package com.example.emergencycall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rumah_sakit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rumah_sakit);
+    }
+}
